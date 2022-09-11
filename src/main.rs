18aad61e586774e1692e2a27de8f94d3b075ac7a
Jack Byrne <jgbyrne9@gmail.com>
2022-09-11 11:06:48 +0100

    crc and symmap

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod bwt;
+mod crc32;
 mod huffman;
 mod mtf;
 mod rle;
@@ -7,10 +8,10 @@ mod out;
 use out::OutputStream;
 
 use std::env;
-use std::process;
 use std::fs;
 use std::io;
 use std::io::Read;
+use std::process;
 
 use crc::Crc;
 
@@ -20,17 +21,47 @@ fn write_stream_header<W: io::Write>(output: &mut OutputStream<W>, level: usize)
     output.write_bytes(&[0x42, 0x5A, 0x68, level_byte])
 }
 
-fn write_block_header<W: io::Write>(output: &mut OutputStream<W>, crc: u32, ptr: usize) -> io::Result<()> {
+fn write_block_header<W: io::Write>(
+    output: &mut OutputStream<W>,
+    crc: u32,
+    ptr: usize,
+) -> io::Result<()> {
     output.write_bytes(&[0x31, 0x41, 0x59, 0x26, 0x53, 0x59])?;
-    /* this needs more thought... need most-significant-bits on left because bzip
     output.write_bytes(&crc.to_be_bytes())?;
-    output.write_bits(1, 1)?;
+    output.write_bits(0, 1)?;
 
     let ptr_bytes = ptr.to_be_bytes();
     let slice_left = ptr_bytes.len() - 3;
     output.write_bytes(&ptr.to_be_bytes()[slice_left..])
-    */
-    unimplemented!();
+}
+
+fn write_sym_map<W: io::Write>(
+    output: &mut OutputStream<W>,
+    has_byte: &Vec<bool>,
+) -> io::Result<()> {
+    let mut sector_map: u16 = 0;
+    let mut sectors: Vec<u16> = vec![];
+
+    for a in 0..16 {
+        sector_map <<= 1;
+        let mut sector = 0;
+        for b in 0..16 {
+            sector <<= 1;
+            let byte = (a << 4) | b;
+            if has_byte[byte] {
+                sector |= 1;
+            }
+        }
+        if sector != 0 {
+            sector_map |= 1;
+            sectors.push(sector);
+        }
+    }
+    assert!(sectors.len() > 0);
+    output.write_bytes(&sector_map.to_be_bytes())?;
+    for s in sectors {
+        output.write_bytes(&s.to_be_bytes())?;
+    }
 }
 
 fn main() {
@@ -68,18 +99,20 @@ fn main() {
 
     // Build and write one block for the moment
 
-    let crc = crc::Crc::<u32>::new(&crc::CRC_32_ISO_HDLC);
-    let chk = crc.checksum(&buffer);
+    let mut sum_buf = buffer.clone();
+    let sum = crc32::checksum(&mut sum_buf);
 
     let (buffer, buffer_len) = rle::rle_one(&buffer, level);
 
     let bwt_out = bwt::bwt(buffer);
 
-    write_block_header(&mut output, chk, bwt_out.ptr).unwrap();
+    write_block_header(&mut output, sum, bwt_out.ptr).unwrap();
+
+    write_sym_map(&mut output, &bwt_out.has_byte);
 
     let mtf_out = mtf::mtf_and_rle(bwt_out.bwt, bwt_out.has_byte);
 
-    //huffman::encode(&mut output, mtf_out).unwrap();
+    huffman::encode(&mut output, mtf_out).unwrap();
 
     output
         .write_bytes(&[0x17, 0x72, 0x45, 0x38, 0x50, 0x90])
