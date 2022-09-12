3ed321d2d7afced32f34f033a0d7128f8b850b9f
Jack Byrne <jgbyrne9@gmail.com>
2022-09-12 11:25:11 +0100

    comments and cleanup for huffman.rs

--- src/huffman.rs
+++ src/huffman.rs
@@ -1,13 +1,20 @@
 // =-=-= huffman.rs =-=-=
 // Implementation of huffman coding for bzip2
-// Largely a port of the original C implementation
+// :: Derived largely from original bzip2 implementation
+// :: (see https://sourceware.org/bzip2)
 
 use crate::mtf;
 use crate::out;
 use std::io;
 use std::ops::Add;
 
+// Current convention is to never encode a symbol longer than 17 bits
 const CODEWORD_MAX_LEN: usize = 17;
+
+// `Table` is a list s.t. table[sym] = |codeword(sym)|
+type Table = Vec<u8>;
+
+// === A singly-linked Binary Tree used for code construction ===
 
 struct TreeNode {
     id: usize,
@@ -21,6 +28,10 @@ struct Tree {
 }
 
 impl Tree {
+    // Create a tree with n leaves and a root, all isolated
+    // :: root is id 0
+    // :: leaves are id 1 to n inclusive
+    // :: thus, leaf i corresponds to symbol (i - 1)
     fn with_n_leaves(n: usize) -> Tree {
         let mut nodes = Vec::with_capacity(n * 2 - 1);
         nodes.push(TreeNode {
@@ -42,6 +53,9 @@ impl Tree {
         tree
     }
 
+    // Tie two nodes of the tree together into an inner node
+    // :: If this is joining the last two subtrees,
+    // :: we connect them to the root node
     fn tie(&mut self, left: usize, right: usize) -> usize {
         if self.nodes.len() == (self.num_leaves * 2 - 1) {
             self.nodes[0].lchild = Some(left);
@@ -58,10 +72,13 @@ impl Tree {
         }
     }
 
-    fn lengths(&self) -> (Table, usize) {
+    // Produce a coding `Table` of lengths
+    // :: Assumes a full-constructed tree
+    fn coding_lengths(&self) -> (Table, usize) {
         let mut lengths = vec![0; self.num_leaves];
         let mut max_len = 0;
 
+        /* stack-based BFS to leaves: stack elems are (id, dist) */
         let mut stack = vec![(0, 0)];
         while let Some((cur, len)) = stack.pop() {
             let cur = &self.nodes[cur];
@@ -69,7 +86,10 @@ impl Tree {
                 stack.push((l, len + 1));
                 stack.push((r, len + 1));
             } else {
+                /* if root has no children the tree wasn't finished */
                 assert!(cur.id != 0);
+
+                /* symbol is one less than leaf id, so (cur.id - 1) */
                 lengths[cur.id - 1] = len as u8;
                 if len > max_len {
                     max_len = len;
@@ -80,6 +100,7 @@ impl Tree {
         (lengths, max_len)
     }
 
+    // Ugly recursive pretty-print for debugging
     #[allow(unused)]
     fn str_node(&self, node: usize, depth: usize) -> String {
         let indent = " |".repeat(depth);
@@ -105,7 +126,20 @@ impl Tree {
     }
 }
 
-// Priority is (freq_sum, max_word_len)
+// === Priority Queue implementation for building coding tree ===
+
+// Elements of the `FrequencyQueue`, which correspond to `Tree` nodes,
+// have a `Priority` which dictates their position in the queue.
+//
+// :: A priority is a pair (sum_frequency, max_dist) where
+//    sum_frequency is the sum frequency of symbols in this subtree
+//    and max_dist is the longest distance to a leaf in this subtree
+//
+// :: The sum_frequency is the main determinant, with max_dist to break ties.
+//
+// :: Since the `FrequencyQueue` is a min-heap:
+//        a < b   =>   a is higher priority than b
+
 #[derive(PartialEq, PartialOrd, Clone, Copy, Debug)]
 struct Priority(usize, u8);
 
@@ -123,12 +157,16 @@ impl Add for Priority {
     }
 }
 
-// Min-Heap for building coding tree
 struct FrequencyQueue {
     heap: Vec<(u16, Priority)>,
 }
 
 impl FrequencyQueue {
+    // Create new frequency queue with `num_syms` symbols
+    // :: The parameter `scaling` is initially 1, but doubles
+    //    until the maximum codeword length is not exceeded
+    // :: Symbol s is recorded as (s + 1) in the queue to
+    //    match the id of the corresponding `TreeNode`
     fn new(num_syms: usize, freqs: &Vec<usize>, scaling: usize) -> Self {
         let mut queue = Self {
             heap: Vec::with_capacity(num_syms),
@@ -140,6 +178,9 @@ impl FrequencyQueue {
         queue
     }
 
+    // We use 1-indexing for the heap
+    // :: This allows us to traverse the heap with binary shifts
+
     #[inline]
     fn item<'s>(&'s mut self, idx: usize) -> &'s mut (u16, Priority) {
         &mut self.heap[idx - 1]
@@ -150,6 +191,7 @@ impl FrequencyQueue {
         &self.heap[idx - 1]
     }
 
+    // Insert `sym` with `priority` into the queue
     fn insert(&mut self, sym: u16, priority: Priority) {
         let init_idx = self.heap.len() + 1;
         self.heap.push((sym, priority));
@@ -178,15 +220,20 @@ impl FrequencyQueue {
         }
     }
 
+    // Extract the root of the heap
     fn extract(&mut self) -> (u16, Priority) {
-        match self.heap.len() {
-            0 => panic!("Tried to extract() from empty heap"),
-            1 => return self.heap.pop().unwrap(),
-            _ => {},
-        }
+        let (sym, priority) = match self.heap.pop() {
+            None => panic!("Tried to extract() from empty heap"),
+            Some(last) => {
+                if self.heap.len() == 0 {
+                    return last;
+                }
+                last
+            },
+        };
 
         let root = *self.item(1);
-        let (sym, priority) = self.heap.pop().unwrap();
+
         *self.item(1) = (sym, priority);
         let heap_size = self.heap.len();
 
@@ -219,9 +266,11 @@ impl FrequencyQueue {
     }
 }
 
+// Build a coding table from a list of symbol frequencies
 fn build_table_from_freqs(num_syms: usize, freqs: &Vec<usize>) -> Table {
     let mut scaling = 1;
 
+    /* Attempt iteratively, rescaling until CODEWORD_MAX_LEN is respected */
     loop {
         let mut tree = Tree::with_n_leaves(num_syms);
         let mut queue = FrequencyQueue::new(num_syms, freqs, scaling);
@@ -231,13 +280,15 @@ fn build_table_from_freqs(num_syms: usize, freqs: &Vec<usize>) -> Table {
             let (sym_two, priority_two) = queue.extract();
 
             let parent = tree.tie(sym_one as usize, sym_two as usize);
+
+            /* if tree is complete */
             if parent == 0 {
                 break;
             }
             queue.insert(parent as u16, priority_one + priority_two);
         }
 
-        let (lengths, max_len) = tree.lengths();
+        let (lengths, max_len) = tree.coding_lengths();
         if max_len <= CODEWORD_MAX_LEN {
             break lengths;
         }
@@ -245,19 +296,25 @@ fn build_table_from_freqs(num_syms: usize, freqs: &Vec<usize>) -> Table {
     }
 }
 
-type Table = Vec<u8>;
+// === Huffman coding algorithm ===
 
+// Initial pseudo-frequencies for coding tables
 const INIT_LEN_HIGH: u8 = 15;
 const INIT_LEN_LOW: u8 = 0;
 
+// Number of table improvement iterations
 const NUM_REFINEMENTS: u8 = 4;
-const SELECTION_WIDTH: usize = 50;
 
+// bzip2 requires a coding selection every 50 bytes
+const SEGMENT_WIDTH: usize = 50;
+
+// Encode the output of an MTF transform and write to the output stream
 pub fn encode<W: io::Write>(output: &mut out::OutputStream<W>, mtf: mtf::Mtf) -> io::Result<()> {
     let input = mtf.output;
     let input_size = input.len();
     let num_syms = mtf.num_syms;
 
+    // Between 2 and 6 tables, using same thresholds as reference implementation
     let num_tables = match num_syms {
         0..=2 => panic!("Too few symbols for huffman::encode();"),
         3..=199 => 2,
@@ -267,11 +324,16 @@ pub fn encode<W: io::Write>(output: &mut out::OutputStream<W>, mtf: mtf::Mtf) ->
         _ => 6,
     };
 
+    // Build initial coding tables
+    // :: The approach is to assign each table a contiguous chunk of the symbol
+    // :: range such that each chunk has approximately equal sum frequency,
+    // :: then initialise each table with a bias for the symbols in its range
+
     let mut tables = vec![Table::with_capacity(num_syms); num_tables];
 
     let mut freq_remaining = input_size;
-
     let mut sym_left = 0;
+
     for cur_table in 0..num_tables {
         let tables_remaining = num_tables - cur_table;
         let freq_target = freq_remaining / tables_remaining;
@@ -287,8 +349,8 @@ pub fn encode<W: io::Write>(output: &mut out::OutputStream<W>, mtf: mtf::Mtf) ->
             sym_right += 1;
         }
 
-        // This strange check backtracks on odd internal tables
-        // to try and neutralise the average 'greediness'
+        // This strange check backtracks one symbol on odd internal
+        // tables to push the average 'greediness' towards zero
         if sym_right > sym_left
             && cur_table != 0
             && cur_table != (num_tables - 1)
@@ -312,57 +374,82 @@ pub fn encode<W: io::Write>(output: &mut out::OutputStream<W>, mtf: mtf::Mtf) ->
         freq_remaining -= freq_acc;
     }
 
+    // The heart of the coding algorithm:
+    // :: Iteratively:
+    //      -> Determine the best table for each 50-byte segment,
+    //         then recalculate the frequency list for each table
+    //         based on the sum symbol frequencies from the segments
+    //         for which it is the best fit.
+    //
+    //      -> Rebuild tables from new frequency lists
+    //
+    // :: On final iteration, push best table ids to selector list
+
+    /* new sum frequencies for each table */
     let mut table_freqs: Vec<Vec<usize>> = vec![];
-    let mut table_sum_lens: Vec<usize> = vec![];
+
+    /* table cost is number of bits required to encode the segment */
+    let mut table_costs: Vec<usize> = vec![0; num_syms];
+
     for _ in 0..num_tables {
         table_freqs.push(vec![0; num_syms]);
-        table_sum_lens.push(0);
     }
 
+    /* populated on final iteration */
     let mut selectors: Vec<usize> = vec![];
 
     for it in 0..NUM_REFINEMENTS {
-        let last_it = it == (NUM_REFINEMENTS - 1);
+        let final_it = it == (NUM_REFINEMENTS - 1);
 
-        for t in 0..num_tables {
-            for s in 0..num_syms {
-                table_freqs[t][s] = 0;
+        /* zero out frequency lists on each iteration */
+        if it != 0 {
+            for t in 0..num_tables {
+                for s in 0..num_syms {
+                    table_freqs[t][s] = 0;
+                }
             }
         }
 
+        /* iterate over segments */
         let mut buf_left = 0;
         let mut buf_right;
         loop {
-            buf_right = buf_left + SELECTION_WIDTH - 1;
+            buf_right = buf_left + SEGMENT_WIDTH - 1;
+
+            /* last segment may be abbreviated */
             if buf_right >= input_size {
                 buf_right = input_size - 1;
             }
 
+            /* zero out length tables */
             for t in 0..num_tables {
-                table_sum_lens[t] = 0;
+                table_costs[t] = 0;
             }
 
-            /* this gonna be slow! */
+            /* accumulate coding costs for each table */
             for s in &input[buf_left..=buf_right] {
                 for t in 0..num_tables {
-                    table_sum_lens[t] += tables[t][*s as usize] as usize;
+                    table_costs[t] += tables[t][*s as usize] as usize;
                 }
             }
 
+            /* best table has lowest cost */
             let mut best_table = 0;
-            let mut best_table_sum_len = usize::MAX;
+            let mut best_table_cost = usize::MAX;
             for t in 0..num_tables {
-                if table_sum_lens[t] < best_table_sum_len {
+                if table_costs[t] < best_table_cost {
                     best_table = t;
-                    best_table_sum_len = table_sum_lens[t];
+                    best_table_cost = table_costs[t];
                 }
             }
 
+            /* segment contributes its frequency to the best table */
             for s in &input[buf_left..=buf_right] {
                 table_freqs[best_table][*s as usize] += 1;
             }
 
-            if last_it {
+            /* on final iteration build selectors list */
+            if final_it {
                 selectors.push(best_table);
             }
 
@@ -372,18 +459,22 @@ pub fn encode<W: io::Write>(output: &mut out::OutputStream<W>, mtf: mtf::Mtf) ->
             }
         }
 
+        /* rebuild tables with new frequency lists */
         for table in 0..num_tables {
             tables[table] = build_table_from_freqs(num_syms, &mut table_freqs[table]);
         }
     }
 
+    // === Write encoded data to the output ===
+
+    // num_tables as 3 bit integer
     output.write_bits(num_tables as u8, 3)?;
 
-    let num_selectors = selectors.len() as u16;
-    let num_sel_bytes = num_selectors.to_be_bytes();
-    output.write_bits(num_sel_bytes[0], 7)?;
-    output.write_byte(num_sel_bytes[1])?;
+    // num_selectors as 15 bit integer
+    let num_selectors = selectors.len() as u32;
+    output.write_bits_u32(num_selectors, 15)?;
 
+    // selectors list is MTF encoded
     let mut selectors_mtf = Vec::with_capacity(num_tables);
     let mut idx_codes = Vec::with_capacity(num_tables);
 
@@ -392,7 +483,7 @@ pub fn encode<W: io::Write>(output: &mut out::OutputStream<W>, mtf: mtf::Mtf) ->
         if i == 0 {
             idx_codes.push(0);
         } else {
-            // 'i' ones followed by a zero
+            /* 'i' ones followed by a zero */
             idx_codes.push((1 << (i + 1)) - 2)
         }
     }
@@ -417,28 +508,37 @@ pub fn encode<W: io::Write>(output: &mut out::OutputStream<W>, mtf: mtf::Mtf) ->
         }
     }
 
+    // Coding tables are delta-encoded
+
     let mut codings = Vec::with_capacity(num_tables);
 
     for table in tables {
         let mut min_len = u8::MAX;
         let mut max_len = 0;
 
+        // Initial coding length is 5-bit integer
         output.write_bits(table[0], 5)?;
+
         let mut acc = table[0];
         for l in table.iter() {
+            // Encode delta from previous length
             loop {
                 if *l == acc {
+                    /* 0 when length matches */
                     output.write_bits(0, 1)?;
                     break;
                 }
                 if acc < *l {
+                    /* 10 means increment */
                     output.write_bits(2, 2)?;
                     acc += 1;
                 } else if acc > *l {
+                    /* 11 means decrement */
                     output.write_bits(3, 2)?;
                     acc -= 1;
                 }
             }
+
             if *l < min_len {
                 min_len = *l;
             }
@@ -446,6 +546,9 @@ pub fn encode<W: io::Write>(output: &mut out::OutputStream<W>, mtf: mtf::Mtf) ->
                 max_len = *l;
             }
         }
+
+        // Construct codewords for this table
+        // :: coding[sym] = (word_length, word)
 
         let mut coding = vec![(0, 0); num_syms];
         let mut word: u32 = 0;
@@ -461,12 +564,7 @@ pub fn encode<W: io::Write>(output: &mut out::OutputStream<W>, mtf: mtf::Mtf) ->
         codings.push(coding);
     }
 
-    /*
-    println!("Encoding Block with {} selectors, {} trees, {} bytes, {} symbols", selectors.len(), num_tables, input.len(), num_syms);
-    for t in 0..num_tables {
-        println!("\t-> Coding table {}: {:?}", t, &codings[t]);
-    }
-    */
+    // Huffman encode input buffer
 
     let mut sel = selectors[0];
     for (i, s) in input.iter().enumerate() {
