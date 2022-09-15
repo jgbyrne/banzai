405e39b5ccf252ce97b29bd6cc46e65b96112a41
Jack Byrne <jgbyrne9@gmail.com>
2022-09-15 15:48:04 +0100

    working towards alpha03

--- src/main.rs
+++ src/main.rs
@@ -9,29 +9,104 @@ use std::fs;
 use std::io::{self, BufWriter, Read};
 use std::process;
 
+const SUCCESS: i32 = 0;
 const ERR_ARGS: i32 = 1;
 const ERR_FILESYSTEM: i32 = 2;
 const ERR_OUTPUT: i32 = 3;
+
+const VERSION: &'static str = concat!("version alpha ", env!("CARGO_PKG_VERSION"));
 
 fn fs_die(e: io::Error) -> ! {
     eprintln!("[filesystem error] {}", e);
     process::exit(ERR_FILESYSTEM);
 }
 
-fn help_die() -> ! {
+fn synopsis_die() -> ! {
     eprintln!("banzai is a libre bzip2 encoder");
     eprintln!("     usage  : banzai file_to_encode");
-    eprintln!("     output : file_to_encode.bz2");
-    eprintln!("version pre-alpha {}", env!("CARGO_PKG_VERSION"));
+    eprintln!("     run 'banzai --help' for full options");
+    eprintln!("{}", VERSION);
     process::exit(ERR_ARGS);
 }
 
+const USAGE_MSG: &'static str = r#"
+     usage: banzai [args] file_to_encode
+
+     -r --replace    delete original file
+     -c --stdout     output to standard out
+     -1 ... -9       set block size (100 to 900 kb)
+     -v --verbose    more extensive logging
+        --help       print this message
+        --info       print information about banzai
+        --version    print version string
+"#;
+
+fn help_die() -> ! {
+    eprintln!("banzai is a libre bzip2 encoder");
+    eprintln!("{}", USAGE_MSG);
+    process::exit(SUCCESS);
+}
+
+const INFO_MSG: &'static str =
+r#"banzai is a libre bzip2 encoder
+
+This program uses the SA-IS algorithm to determine
+the Burrows-Wheeler Transform, while codeword lengths
+for Huffman Encoding are chosen by iterative refinement.
+
+It is asymptotically linear-time in the input and is
+implemented wholly in safe, modern, and idiomatic Rust.
+
+However, it is currently in the alpha stage and should
+not be used as part of a production software system.
+
+This software is written and maintained by Jack Byrne,
+and released for public use under the MIT license.
+"#;
+
+fn info_die() -> ! {
+    eprintln!("{}", INFO_MSG);
+    eprintln!("{}", VERSION);
+    process::exit(SUCCESS);
+}
+
+fn version_die() -> ! {
+    eprintln!("{}", VERSION);
+    process::exit(SUCCESS);
+}
+
+enum ArgExpect {
+    Any,
+}
+
 fn main() {
-    let mut args = env::args().skip(1);
-    if args.len() > 1 {
-        help_die()
+    let args = env::args().skip(1);
+
+    let mut path = None;
+
+    let mut exp = ArgExpect::Any;
+    for a in args {
+        match exp {
+            ArgExpect::Any => {
+                if a.starts_with('-') {
+                    match a.as_str() {
+                        "--help" => help_die(),
+                        "--version" => version_die(),
+                        "--info" => info_die(),
+                        _ => {
+                            eprintln!("This is another message");
+                            process::exit(ERR_ARGS);
+                        }
+                    }
+                }
+                else {
+                    path = Some(a);
+                }
+            }
+        }
     }
-    let path = args.next().unwrap_or_else(|| help_die());
+
+    let path = path.unwrap_or_else(|| synopsis_die());
 
     let mut file = fs::File::open(&path).unwrap_or_else(|err| fs_die(err));
 
